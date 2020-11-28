0cb074f3bc5ff252dc827a83a6a5c7aef8d4df2e
Suhas KS <suhas.ks@reverieinc.com>
2020-11-28 15:11:06 +0530

    update

--- templates/basic.py
+++ templates/basic.py
@@ -2,15 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('basic.html')
+    return '<h1>welcome!!</h1>'
 
-
-
-
-
-
+@app.route('/<name>')
+def name(name):
+    letters = list(name)
+    dicts = {'name':name.upper()}
+    return render_template('basic.html', name=name.upper(), letters=letters, dicts=dicts)
 
 if __name__=='__main__':
     app.run()
---
+++ templates/basic2.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    mylist = [1,2,3,4,5]
+    return render_template('basic2.html', mylist=mylist)
+
+if __name__=='__main__':
+    app.run(debug=True)
