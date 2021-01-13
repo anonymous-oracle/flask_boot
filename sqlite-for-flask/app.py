30c01f9846609d403b2b04b460412976bbe66d7c
Suhas KS <suhass149@gmail.com>
2021-01-13 21:42:32 +0530

    update

--- sqlite-for-flask/app.py
+++ sqlite-for-flask/app.py
@@ -1,78 +1,21 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
-from werkzeug.exceptions import TooManyRequests
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 
 from security import authenticate, identity
 from user import UserRegister
+from item import Item, ItemList
 
 app = Flask(__name__)
-app.secret_key = 'secret'
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'jose'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)  # /auth
+jwt = JWT(app, authenticate, identity)
 
-
-# storing items in a list
-items = []
-
-
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
-
-
-class Item(Resource):
-    # the parser belongs to the class and not just one specific Item object/resource
-    # determines what fields of the payload are used to update the item
-    parser = reqparse.RequestParser()
-    parser.add_argument('price', type=float, required=True,
-                        help='This field cannot be left blank')
-
-    @jwt_required()  # can put this decorator on any of the methods below and they would require authorization
-    def get(self, name):
-        item = next(filter(lambda item: item['name'] == name, items), None)
-        return {'item': item}, 200 if item else 404
-
-    def post(self, name):
-        # to ensure that the item is not repeated
-        if next(filter(lambda item: item['name'] == name, items), None):
-            return {'message': 'item with name %s already exists' % (name)}, 400
-
-        data = request.get_json()
-        newItem = {'name': name, 'price': data['price']}
-        items.append(newItem)
-        return newItem, 201
-
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted'}
-
-    def put(self, name):
-        # the .parse_args() will allow only the added arguments and remove the others in the payload
-        data = Item.parser.parse_args()
-
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item == None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
-
-
-# https://127.0.0.1:5000/student/<name>
-# api.add_resource(Student, '/student/<name>')
+api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-api.add_resource(Item, '/item/<name>')
 api.add_resource(UserRegister, '/register')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # important to mention debug=True
--- sqlite-for-flask/user.py
+++ sqlite-for-flask/user.py
@@ -1,22 +1,23 @@
+import sqlite3
 from flask_restful import Resource, reqparse
-import sqlite3 as sql
 
-class User:
+
+class User():
+    TABLE_NAME = 'users'
+
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
 
-
-    # converts the following to a class method instead of an object/resource method
     @classmethod
     def find_by_username(cls, username):
-        connection = sql.connect('data.db')
+        connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
-        query = 'select * from users where username=?'
+        query = "SELECT * FROM {table} WHERE username=?".format(table=cls.TABLE_NAME)
         result = cursor.execute(query, (username,))
-        row=result.fetchone()
+        row = result.fetchone()
         if row:
             user = cls(*row)
         else:
@@ -27,12 +28,12 @@ class User:
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sql.connect('data.db')
+        connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
-        query = 'select * from users where id=?'
+        query = "SELECT * FROM {table} WHERE id=?".format(table=cls.TABLE_NAME)
         result = cursor.execute(query, (_id,))
-        row=result.fetchone()
+        row = result.fetchone()
         if row:
             user = cls(*row)
         else:
@@ -41,28 +42,35 @@ class User:
         connection.close()
         return user
 
+
 class UserRegister(Resource):
+    TABLE_NAME = 'users'
+
     parser = reqparse.RequestParser()
     parser.add_argument('username',
-    type=str,
-    required=True,
-    help="This field cannot be blank.")
-    
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
     parser.add_argument('password',
-    type=str,
-    required=True,
-    help="This field cannot be blank.")
-    
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+
     def post(self):
         data = UserRegister.parser.parse_args()
-        # if User.find_by_username(data['username'])
 
-        connection=sql.connect('data.db')
+        if User.find_by_username(data['username']):
+            return {"message": "User with that username already exists."}, 400
+
+        connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
-        query = 'insert into users values (NULL, ?, ?)'
+        query = "INSERT INTO {table} VALUES (NULL, ?, ?)".format(table=self.TABLE_NAME)
         cursor.execute(query, (data['username'], data['password']))
 
         connection.commit()
         connection.close()
-        return {'message': 'User created successfully'}, 201
+
+        return {"message": "User created successfully."}, 201
